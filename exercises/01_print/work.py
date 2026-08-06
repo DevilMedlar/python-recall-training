86b6b60d33b4468f87610a3e65241dcb5d00710d
DevilMedlar <202556815+DevilMedlar@users.noreply.github.com>
2026-08-05 21:57:56 -0400

    Add motivational print statements to work.py

--- exercises/01_print/work.py
+++ exercises/01_print/work.py
@@ -1,3 +1,4 @@
 # Write only your current challenge attempt below this line.
-print("Python is real code.")
-print("I typed this myself.")
+print("I'm learning Python.")
+print("Errors are clues.")
+print("Keep typing!")
