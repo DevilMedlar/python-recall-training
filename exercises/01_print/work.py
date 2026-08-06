18fe4513248ed36cbbdb3d7cdbce9a617d34ff94
DevilMedlar <202556815+DevilMedlar@users.noreply.github.com>
2026-08-05 22:01:38 -0400

    Update print statements in work.py

--- exercises/01_print/work.py
+++ exercises/01_print/work.py
@@ -1,4 +1,4 @@
 # Write only your current challenge attempt below this line.
-print("I'm learning Python.")
-print("Errors are clues.")
-print("Keep typing!")
+print("First line.")
+print("")
+print("Third line.")
