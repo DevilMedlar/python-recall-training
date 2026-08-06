4fdcae1ba9e182a3449cc296cdf6e2374f0c1cdb
DevilMedlar <znorris1994@gmail.com>
2026-08-05 22:05:23 -0400

    Add print statements for new challenge attempts

--- exercises/01_print/work.py
+++ exercises/01_print/work.py
@@ -1,4 +1,3 @@
 # Write only your current challenge attempt below this line.
-print("First line.")
-print("")
-print("Third line.")
+print("She said, \"Keep going.\"")
+print("Python won't scare me.")
