6b93185e35c64d48fdcb88a41ff44d4972e7dbc6
DevilMedlar <znorris1994@gmail.com>
2026-08-07 01:45:28 -0400

    Update work.py

--- exercises/02_variables/work.py
+++ exercises/02_variables/work.py
@@ -1,1 +1,10 @@
 # Write only your current Stage 02 challenge attempt below this line.
+first_text="First variable."
+second_text="Second variable."
+third_text="Third variable."
+
+print(third_text)
+print(first_text)
+print(second_text)
+print(third_text)
+print(first_text)
