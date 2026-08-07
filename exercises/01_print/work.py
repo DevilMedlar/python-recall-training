7d1e8fe3d6098fcae4d045886873b596d50263d9
DevilMedlar <znorris1994@gmail.com>
2026-08-07 01:17:53 -0400

    Fix print function call in work.py

--- exercises/01_print/work.py
+++ exercises/01_print/work.py
@@ -1,6 +1,6 @@
 # Write only y"ur Recall Check 02 attempt below this line.
 print("[FINAL RECALL]")
-Print("\"Memory beats copying.\"")
+print("\"Memory beats copying.\"")
 print("Don't peek.")
 print("")
 print("Stage 01: almost done!")
