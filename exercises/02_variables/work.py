f1a8aa0596162198674fd927716b9fdb4751367d
DevilMedlar <202556815+DevilMedlar@users.noreply.github.com>
2026-08-07 01:28:27 -0400

    Add print statements for Stage 02 challenge

--- exercises/02_variables/work.py
+++ exercises/02_variables/work.py
@@ -1,1 +1,5 @@
 # Write only your current Stage 02 challenge attempt below this line.
+first_line="Memory grows with repetition."
+second_line="Variables have names."
+print(first_line)
+print(second_line)
