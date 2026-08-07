1c3a9e6ecc3cf8a5aebcedac95dac889a123c6ed
DevilMedlar <znorris1994@gmail.com>
2026-08-07 02:17:38 -0400

    Update work.py

--- exercises/02_variables/work.py
+++ exercises/02_variables/work.py
@@ -1,1 +1,12 @@
 # Write only your current Stage 02 challenge attempt below this line.
+header="[VARIABLE CAPSTONE]"
+message="She said, \"Keep the name.\""
+state="Waiting."
+print(message)
+print(header)
+print(state)
+state="Running."
+print("")
+print(header)
+print(state)
+print(message)
