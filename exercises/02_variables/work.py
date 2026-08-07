938837b0765bca38882ea13946d885aef660e2a7
DevilMedlar <202556815+DevilMedlar@users.noreply.github.com>
2026-08-07 01:41:52 -0400

    Reset work file for Stage 02 Challenge 06

--- exercises/02_variables/work.py
+++ exercises/02_variables/work.py
@@ -1,7 +1,1 @@
 # Write only your current Stage 02 challenge attempt below this line.
-header="[VARIABLE DRILL]"
-quote="She said, \"Store this.\""
-print(header)
-print(quote)
-print("")
-print(quote)
