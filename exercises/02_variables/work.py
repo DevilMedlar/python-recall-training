4abc94fd3000ae128ab1f284dad9ce5d69f4514b
DevilMedlar <znorris1994@gmail.com>
2026-08-07 01:29:15 -0400

    Reset work file for Stage 02 Challenge 03

--- exercises/02_variables/work.py
+++ exercises/02_variables/work.py
@@ -1,5 +1,1 @@
 # Write only your current Stage 02 challenge attempt below this line.
-first_line="Memory grows with repetition."
-second_line="Variables have names."
-print(first_line)
-print(second_line)
