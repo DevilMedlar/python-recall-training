a1923d8bcaed7fa1e573c424adf2fc0f545c8783
DevilMedlar <znorris1994@gmail.com>
2026-08-07 01:36:30 -0400

    Update work.py

--- exercises/02_variables/work.py
+++ exercises/02_variables/work.py
@@ -1,1 +1,6 @@
 # Write only your current Stage 02 challenge attempt below this line.
+first_message="First comes second."
+second_message="Second comes first."
+print(second_message)
+print(first_message)
+print(second_message)
