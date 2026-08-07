0f6eac99cfc348edd3521b57e50957aa5199d355
DevilMedlar <znorris1994@gmail.com>
2026-08-07 01:14:57 -0400

    Fix typo and add print statements in work.py

--- exercises/01_print/work.py
+++ exercises/01_print/work.py
@@ -1,1 +1,6 @@
-# Write only your Recall Check 02 attempt below this line.
+# Write only y"ur Recall Check 02 attempt below this line.
+pritn([FINAL RECALL]")
+Print("\"Memory beats copying.\"")
+print("Don't peek.")
+print("")
+print("Stage 01: almost done!)
