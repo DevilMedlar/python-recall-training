33238991924d4ae15361c817fd9f2c826e976cee
DevilMedlar <znorris1994@gmail.com>
2026-08-07 01:40:55 -0400

    Add variable drill example with header and quote

--- exercises/02_variables/work.py
+++ exercises/02_variables/work.py
@@ -1,1 +1,7 @@
 # Write only your current Stage 02 challenge attempt below this line.
+header="[VARIABLE DRILL]"
+quote="She said, \"Store this.\""
+print(header)
+print(quote)
+print("")
+print(quote)
