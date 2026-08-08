56c8720b264a1b2ea98af3f4ab6c44b6d082dc41
DevilMedlar <znorris1994@gmail.com>
2026-08-08 08:31:44 -0400

    Reset work file for Stage 02 Recall Check 01

--- exercises/02_variables/work.py
+++ exercises/02_variables/work.py
@@ -1,12 +1,1 @@
-# Write only your current Stage 02 challenge attempt below this line.
-header="[VARIABLE CAPSTONE]"
-message="She said, \"Keep the name.\""
-state="Waiting."
-print(message)
-print(header)
-print(state)
-state="Running."
-print("")
-print(header)
-print(state)
-print(message)
+# Write only your current Stage 02 Recall Check 01 attempt below this line.
