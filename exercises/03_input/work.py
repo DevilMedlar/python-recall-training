0f40d7c65b9c659111c475e4368ea9b4fcd3fddc
DevilMedlar <202556815+DevilMedlar@users.noreply.github.com>
2026-08-09 00:35:49 -0400

    Update work.py

--- exercises/03_input/work.py
+++ exercises/03_input/work.py
@@ -1,1 +1,8 @@
 # Write only your current Stage 03 challenge attempt below this line.
+label="[INPUT STATUS]"
+state=input("First state: ")
+print(label)
+print(state)
+state=input("Second state: ")
+print(label)
+print(state)
