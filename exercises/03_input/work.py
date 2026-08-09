ed68a8304ece9ac0fe3c3c2983a3eb1969a3a345
DevilMedlar <znorris1994@gmail.com>
2026-08-09 00:49:11 -0400

    Add input prompts and print statements for user input

--- exercises/03_input/work.py
+++ exercises/03_input/work.py
@@ -1,1 +1,12 @@
 # Write only your current Stage 03 challenge attempt below this line.
+header="[INPUT CAPSTONE]"
+message=input("Message: ")
+state=input("State one: ")
+print(message)
+print(header)
+print(state)
+print("")
+state=input("State two: ")
+print(header)
+print(state)
+print(message)
