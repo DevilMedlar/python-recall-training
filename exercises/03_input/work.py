3d37295fe1fbd20f42eaade747e04759f92a6160
DevilMedlar <znorris1994@gmail.com>
2026-08-09 00:36:35 -0400

    Reset work file for Stage 03 Challenge 09

--- exercises/03_input/work.py
+++ exercises/03_input/work.py
@@ -1,8 +1,1 @@
 # Write only your current Stage 03 challenge attempt below this line.
-label="[INPUT STATUS]"
-state=input("First state: ")
-print(label)
-print(state)
-state=input("Second state: ")
-print(label)
-print(state)
