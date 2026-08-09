4fcf2912859fd4ac6203535547ee6bff4b5e83cd
DevilMedlar <202556815+DevilMedlar@users.noreply.github.com>
2026-08-09 00:42:40 -0400

    Reset work file for Stage 03 Challenge 10

--- exercises/03_input/work.py
+++ exercises/03_input/work.py
@@ -1,10 +1,1 @@
 # Write only your current Stage 03 challenge attempt below this line.
-header="[INPUT MEMORY]"
-first_line=input("First line: ")
-second_line=input("Second line: ")
-print(header)
-print(second_line)
-print("")
-print(first_line)
-print(header)
-print(second_line)
