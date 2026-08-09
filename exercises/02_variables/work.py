ad3b6b7a7e8e292ddab1ea954154af76a1c8faf0
DevilMedlar <znorris1994@gmail.com>
2026-08-09 00:07:25 -0400

    Update work.py

--- exercises/02_variables/work.py
+++ exercises/02_variables/work.py
@@ -1,1 +1,13 @@
 # Write only your current Stage 02 Recall Check 02 attempt below this line.
+tag="[FINAL VARIABLE CHECK]"
+line="She said, \"Recall the name.\""
+mode="Before."
+print(line)
+print(tag)
+print(mode)
+mode="After!"
+print("")
+print(tag)
+print(mode)
+print(line)
+print(tag)
