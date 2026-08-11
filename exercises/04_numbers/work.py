fd57cf0ce344d6ca3881933bc5d1303209bb3994
DevilMedlar <202556815+DevilMedlar@users.noreply.github.com>
2026-08-11 08:35:34 -0400

    Add input handling and print statements for numbers

--- exercises/04_numbers/work.py
+++ exercises/04_numbers/work.py
@@ -1,1 +1,12 @@
 # Write only your current Stage 04 challenge attempt below this line.
+tag="[INTEGER LOG]"
+first_text=input("First value: ")
+first_number=int(first_text)
+second_text=input("Second value: ")
+second_number=int(second_text)
+print(tag)
+print(second_number)
+print(first_number)
+print("")
+print(second_number)
+print(tag)
