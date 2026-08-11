8ae90ade338d52dc09bc126fcbbb20e19de32da4
DevilMedlar <znorris1994@gmail.com>
2026-08-11 09:03:29 -0400

    Reset work file for Stage 04 Challenge 08

--- exercises/04_numbers/work.py
+++ exercises/04_numbers/work.py
@@ -1,12 +1,1 @@
 # Write only your current Stage 04 challenge attempt below this line.
-tag="[NUMBER STATE]"
-text=input("First integer: ")
-number=int(text)
-print(tag)
-print(number)
-text=input("Second integer: ")
-number=int(text)
-print(number)
-print("")
-print(tag)
-print(number)
