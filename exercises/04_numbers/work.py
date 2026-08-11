b8a6e3cd985c97d6a94983171469f38ae2ed79d5
DevilMedlar <202556815+DevilMedlar@users.noreply.github.com>
2026-08-11 08:25:13 -0400

    Reset Stage 04 work for Challenge 05

--- exercises/04_numbers/work.py
+++ exercises/04_numbers/work.py
@@ -1,7 +1,1 @@
 # Write only your current Stage 04 challenge attempt below this line.
-text=input("First number: ")
-number=int(text)
-print(number)
-text=input("Second number: ")
-number=int(text)
-print(number)
