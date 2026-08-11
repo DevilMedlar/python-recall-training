c8b8b45691aa9f1f38d2b32b99577a8c2a03414a
DevilMedlar <202556815+DevilMedlar@users.noreply.github.com>
2026-08-11 08:30:56 -0400

    Reset Stage 04 work for Challenge 06

--- exercises/04_numbers/work.py
+++ exercises/04_numbers/work.py
@@ -1,9 +1,1 @@
 # Write only your current Stage 04 challenge attempt below this line.
-label="[NUMBER MEMORY]"
-text=input("Value: ")
-number=int(text)
-print(label)
-print(number)
-print("")
-print(number)
-print(label)
