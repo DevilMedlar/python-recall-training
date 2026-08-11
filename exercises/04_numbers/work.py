c8cee6e7e5b059781bb5f851ac7e4bef16ea7dbc
DevilMedlar <znorris1994@gmail.com>
2026-08-11 08:28:40 -0400

    Implement number memory functionality in work.py

--- exercises/04_numbers/work.py
+++ exercises/04_numbers/work.py
@@ -1,1 +1,9 @@
 # Write only your current Stage 04 challenge attempt below this line.
+label="[NUMBER MEMORY]"
+text=input("Value: ")
+number=int(text)
+print(label)
+print(number)
+print("")
+print(number)
+print(label)
