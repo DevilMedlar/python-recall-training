c6d78dad6fbfa13947061f9ed36a959f959eb0ac
DevilMedlar <znorris1994@gmail.com>
2026-08-11 07:41:25 -0400

    Implement input and print functionality in work.py

--- exercises/03_input/work.py
+++ exercises/03_input/work.py
@@ -1,1 +1,14 @@
 # Write only your current Stage 03 Recall Check 02 attempt below this line.
+tag="[FINAL INPUT RECALL]"
+line=input("Memory line: ")
+mode=input("Current mode: ")
+print(line)
+print(tag)
+print(mode)
+print()
+print(tag)
+print(line)
+mode=input("Next mode: ")
+print(mode)
+print(line)
+print(tag)
