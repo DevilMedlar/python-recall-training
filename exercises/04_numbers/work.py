cf7624854ce91b270063ff856510c9fcabb42c6b
DevilMedlar <znorris1994@gmail.com>
2026-08-11 08:12:07 -0400

    Update work.py

--- exercises/04_numbers/work.py
+++ exercises/04_numbers/work.py
@@ -1,1 +1,7 @@
 # Write only your current Stage 04 challenge attempt below this line.
+first_text=input("First number: ")
+first_number=int(first_text)
+second_text=input("Second number: ")
+second_number=int(second_text)
+print(second_number)
+print(first_number)
