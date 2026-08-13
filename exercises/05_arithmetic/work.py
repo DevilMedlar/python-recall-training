7c9b234d3c33ef33991e632361fa708ff6acbd13
DevilMedlar <znorris1994@gmail.com>
2026-08-13 00:21:58 -0400

    Reset Stage 05 work for Challenge 02

--- exercises/05_arithmetic/work.py
+++ exercises/05_arithmetic/work.py
@@ -1,7 +1,1 @@
 # Write only your current Stage 05 challenge attempt below this line.
-first_text=input("First addend: ")
-first_number=int(first_text)
-second_text=input("Second addend: ")
-second_number=int(second_text)
-total=first_number + second_number
-print(total)
