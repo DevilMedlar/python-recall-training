9e0b0a3d72d47f56f033a66d3cb69d71aa85d2e6
DevilMedlar <znorris1994@gmail.com>
2026-08-12 20:37:51 -0400

    Implement number recall functionality in work.py
    Added input prompts for number recalls and print statements.

--- exercises/04_numbers/work.py
+++ exercises/04_numbers/work.py
@@ -1,2 +1,17 @@
 # Stage 04 Recall Check 01
 # Write only your attempt below this line.
+banner=[INTEGER RECALL]
+text=input("First recall: ")
+first_number=int(text)
+text=input("Current recall: ")
+number=int(text)
+print(first_number)
+print(banner)
+print(number)
+print(\n)
+print(banner)
+text=input("Next recall: ")
+number=int(text)
+print(number)
+print(first_number)
+print(banner)
