aeddd600570e3a22977976af9ea5f549b17d66e0
DevilMedlar <202556815+DevilMedlar@users.noreply.github.com>
2026-08-13 00:10:35 -0400

    Add integer recall functionality in work.py

--- exercises/04_numbers/work.py
+++ exercises/04_numbers/work.py
@@ -1,2 +1,17 @@
 # Stage 04 Recall Check 02
 # Write only your attempt below this line.
+tag="[FINAL INTEGER RECALL]"
+text=input("Anchor number: ")
+first_number=int(text)
+text=input("Current number: ")
+number=int(text)
+print(number)
+print(first_number)
+print(tag)
+text=input("Next number: ")
+number=int(text)
+print(tag)
+print(number)
+print("")
+print(first_number)
+print(tag)
