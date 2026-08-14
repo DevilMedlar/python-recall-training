56c975a044f9bf0a6d9a3e90066d1814089d3703
DevilMedlar <znorris1994@gmail.com>
2026-08-14 10:06:22 -0400

    Add arithmetic operations for user input numbers
    Implement basic arithmetic operations on two user inputs.

---
+++ stages/01-foundations/challenge_14.py
@@ -0,0 +1,12 @@
+you=input("First number:")
+are=int(you)
+so=input("Second non-zero number: ")
+beautiful=int(so)
+senpai=are + beautiful
+print(senpai)
+senpai=are - beautiful
+print(senpai)
+senpai=are * beautiful
+print(senpai)
+senpai=are / beautiful
+print(senpai)
