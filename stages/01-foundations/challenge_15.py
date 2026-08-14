119a768482b5648df221579308e7396b208e8273
DevilMedlar <znorris1994@gmail.com>
2026-08-14 10:33:44 -0400

    Add comparison operations for two integers

---
+++ stages/01-foundations/challenge_15.py
@@ -0,0 +1,10 @@
+num_one=input("First integer: ")
+num_two=input("Second integer: ")
+fuck=int(num_one)
+me=int(num_two)
+senpai=fuck > me
+print(senpai)
+senpai=fuck > me
+print(senpai)
+senpai=fuck == me
+print(senpai)
